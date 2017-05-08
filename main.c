d9f4a0fae6af403585b73dd0a008fb813238bbeb
m.slaterpfl <michael.slater@photonfire.com>
2017-05-08 14:09:14 +0100

    Added new Function
    Removed hello world printing
    Added integer summing function
    added print integer result

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int Adder(int A, int B)
+{
+    //Adds two integers, A and B
+    return A + B;
+}
+
+
 int main()
 {
-    printf("Hello World!\n");
+    int Result = Adder(5, 6);
+
+    printf("Result = %d\n", Result);
     return 0;
 }
